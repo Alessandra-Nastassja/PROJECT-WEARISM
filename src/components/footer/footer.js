38e531be1ce5515efc3348f8fa8b4071baef54ca
alessandra.nsantos <alessandra.nsantos@b2wdigital.com>
2020-04-25 11:08:43 -0300

    Create footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,7 +5,14 @@ import "./footer.scss";
 const Footer = () => {
     return (
         <>
-            footer
+            <footer>
+                <div className="content-footer">
+                    <p>Feito com 
+                        <i className="fas fa-heart"></i>
+                    </p>
+                    <p>© Wearism 2020</p>
+                </div>
+            </footer>
         </>
     )
 }
